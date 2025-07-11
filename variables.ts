ad2671ac7b6d661b78bddcf330a76c8735354b1d
Rafi Uzzaman <rafiyasir9@gmail.com>
2025-07-11 18:28:20 +0530

    24. Delayed Initialization

--- variables.ts
+++ variables.ts
@@ -27,3 +27,12 @@ const logNumber: (i: number) => void = (i: number) => {
 const json = '{"x": 10, "y":20}';
 const coordinates: { x: number; y: number } = JSON.parse(json);
 console.log(coordinates);
+
+let words = ["red", "green", "blue"];
+let foundWord = false;
+
+for (let i = 0; i < words.length; i++) {
+	if (words[i] === "green") {
+		foundWord = true;
+	}
+}
