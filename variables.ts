9ff281d9b8191840a60eb1e706ce4272ccbf4e99
Rafi Uzzaman <rafiyasir9@gmail.com>
2025-07-11 17:57:26 +0530

    19. Object literal Annotations

--- variables.ts
+++ variables.ts
@@ -6,3 +6,16 @@ let nothingMuch: null = null;
 let nothing: undefined = undefined;
 
 let now: Date = new Date();
+
+let colors: string[] = ["red", "green", "blue"];
+let myNumbers: number[] = [1, 2, 3];
+let truths: boolean[] = [true, false, true];
+
+class Car {}
+
+let car: Car = new Car();
+
+let point: { x: number; y: number } = {
+	x: 10,
+	y: 20,
+};
