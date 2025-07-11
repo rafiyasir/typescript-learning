a2cdda60171d9219e2e77ec01619f7bb1652e44a
Rafi Uzzaman <rafiyasir9@gmail.com>
2025-07-11 18:24:05 +0530

    23.Fixing The 'Any' Type

--- variables.ts
+++ variables.ts
@@ -25,5 +25,5 @@ const logNumber: (i: number) => void = (i: number) => {
 };
 
 const json = '{"x": 10, "y":20}';
-const coordinates = JSON.parse(json);
+const coordinates: { x: number; y: number } = JSON.parse(json);
 console.log(coordinates);
