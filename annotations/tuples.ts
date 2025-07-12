3254d37147493f778812a5da01b72ee17176391a
rafi-pawzeeble <rafi@pawzeeble.coom>
2025-07-12 16:42:06 +0530

    38.Why Tuples?

--- annotations/tuples.ts
+++ annotations/tuples.ts
@@ -10,3 +10,10 @@ const pepsi: Drink = ["brown", true, 40];
 // pepsi[0] = 40
 const sprite: Drink = ["clear", true, 40];
 const tea: Drink = ["brown", false, 0];
+
+const carSpecs: [number, number] = [400, 3354];
+
+const carStats = {
+	horsepower: 400,
+	weight: 3354,
+};
