db202088023aa6d3a3dd5ba99bc5186fb089e84c
rafi-pawzeeble <rafi@pawzeeble.coom>
2025-07-12 12:32:58 +0530

    30. Destructuring with Annotations

--- annotations/functions.ts
+++ annotations/functions.ts
@@ -23,3 +23,15 @@ const logger = (message: string): void => {
 const throwError = (message: string): never => {
 	throw new Error(message);
 };
+
+const todaysWeather = {
+	date: new Date(),
+	weather: "sunny",
+};
+
+const logWeather = ({ date, weather }: { date: Date; weather: string }) => {
+	console.log(date);
+	console.log(weather);
+};
+
+logWeather(todaysWeather);
