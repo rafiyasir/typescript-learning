5fbf299a95fdb4d9661c756d60ca7e1f3a5099a1
Rafi Uzzaman <rafiyasir9@gmail.com>
2025-07-12 11:21:46 +0530

    27. Inference Around Functions

--- annotations/functions.ts
+++ annotations/functions.ts
@@ -1,3 +1,8 @@
-const add = (a: number, b: number): number => {
+const add = (a: number, b: number) => {
 	return a + b;
+};
+
+//always use return type not rely on Inference for better checking in return
+const substract = (a: number, b: number): number => {
+	return a - b;
 };
