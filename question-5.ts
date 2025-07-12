d01cde6c5d342ac833aacc9ce7a0574e8ef291ee
rafi-pawzeeble <rafi@pawzeeble.coom>
2025-07-12 19:50:25 +0530

    question 5

---
+++ question-5.ts
@@ -0,0 +1,15 @@
+interface Todo {
+	id: number;
+	name: string;
+	completed: boolean;
+}
+
+interface Model {
+	id: number;
+}
+
+const washDishes = {
+	id: 20,
+	name: "Do the dishes",
+	completed: false,
+};
