061952623b825b300e3b6f3c3dafb7d637b749db
rafi-pawzeeble <rafi@pawzeeble.coom>
2025-07-16 22:07:08 +0530

    Sorter Scaffolding

--- sort/build/index.js
+++ sort/build/index.js
@@ -1,6 +1,10 @@
 "use strict";
-console.log("hi there");
-const hello = () => {
-    console.log("hello");
-};
-hello();
+class Sorter {
+    constructor(collection) {
+        this.collection = collection;
+    }
+    sort() { }
+}
+const sorter = new Sorter([10, 3, 0, 4]);
+sorter.sort();
+console.log(sorter.collection);
--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,5 +1,8 @@
-console.log("hi there");
-const hello = () => {
-	console.log("hello");
-};
-hello();
+class Sorter {
+	constructor(public collection: number[]) {}
+	sort(): void {}
+}
+
+const sorter = new Sorter([10, 3, 0, 4]);
+sorter.sort();
+console.log(sorter.collection);
