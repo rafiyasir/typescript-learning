ff27753e1c3b78ba0846f8a0aa1ad8855ad42a08
Rafi Uzzaman <rafiyasir9@gmail.com>
2025-07-16 16:21:14 +0530

    69. Adding Markers

--- maps/src/CustomMap.ts
+++ maps/src/CustomMap.ts
@@ -1,3 +1,6 @@
+import { Company } from "./Company";
+import { User } from "./User";
+
 export class CustomMap {
 	private googleMap: google.maps.Map;
 
@@ -13,4 +16,15 @@ export class CustomMap {
 			}
 		);
 	}
+
+	addUserMarker(user: User): void {
+		new google.maps.Marker({
+			map: this.googleMap,
+			position: {
+				lat: user.location.lat,
+				lng: user.location.lng,
+			},
+		});
+	}
+	addCompanyMarker(company: Company): void {}
 }
