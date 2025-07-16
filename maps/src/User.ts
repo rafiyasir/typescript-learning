57fc990bf1ed409b333c4ab88a3433f61a8c9a96
rafi-pawzeeble <rafi@pawzeeble.coom>
2025-07-16 17:57:11 +0530

    76. Optional Implements Clauses

--- maps/src/User.ts
+++ maps/src/User.ts
@@ -1,10 +1,12 @@
 import { faker } from "@faker-js/faker";
-export class User {
+import { Mappable } from "./CustomMap";
+export class User implements Mappable {
 	name: string;
 	location: {
 		lat: number;
 		lng: number;
 	};
+	color: string = "red";
 	constructor() {
 		this.name = faker.person.firstName();
 		this.location = {
