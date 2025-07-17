d190bb504ea5a8b458a13dd61f851dc61aaf23d2
rafi-pawzeeble <rafi@pawzeeble.coom>
2025-07-17 17:46:07 +0530

    88. Extracting Key Logic

---
+++ sort/build/sorter.js
@@ -0,0 +1,24 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Sorter = void 0;
+class Sorter {
+    constructor(collection) {
+        this.collection = collection;
+    }
+    sort() {
+        const { length } = this.collection;
+        for (let i = 0; i < length; i++) {
+            for (let j = 0; j < length - i - 1; j++) {
+                if (this.collection[j] > this.collection[j + 1]) {
+                    const temp = this.collection[j];
+                    this.collection[j] = this.collection[j + 1];
+                    this.collection[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
+exports.Sorter = Sorter;
+const sorter = new Sorter([10, 3, 0, 4]);
+sorter.sort();
+console.log(sorter.collection);
