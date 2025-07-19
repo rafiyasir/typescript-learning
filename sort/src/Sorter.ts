ff949d2faaaee26167d4bd729709c870b62c5e99
rafi-pawzeeble <rafi@pawzeeble.coom>
2025-07-19 11:26:57 +0530

    99. Why Use Abstract Classes

--- sort/src/Sorter.ts
+++ sort/src/Sorter.ts
@@ -6,7 +6,11 @@ interface Sortable {
 	swap(leftIndex: number, rightIndex: number): void;
 }
 
-export class Sorter {
+export abstract class Sorter {
+	abstract compare(leftIndex: number, rightIndex: number): boolean;
+	abstract swap(leftIndex: number, rightIndex: number): void;
+	abstract length: number;
+
 	sort(): void {
 		const { length } = this;
 		for (let i = 0; i < length; i++) {
