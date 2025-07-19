463dd3b7797e3716baaf11dabef898453c5c5365
rafi-pawzeeble <rafi@pawzeeble.coom>
2025-07-19 11:32:06 +0530

    100. Solving All Issues with Abstract Classes

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -4,13 +4,15 @@ import { NumbersCollection } from "./NumbersCollection";
 import { Sorter } from "./sorter";
 
 const numbersCollection = new NumbersCollection([10, 3, 0, 4]);
-const sorter = new Sorter(numbersCollection);
-sorter.sort();
+// const sorter = new Sorter(numbersCollection);
+// sorter.sort();
+numbersCollection.sort();
 console.log(numbersCollection.data);
 
 const charactersCollection = new CharactersCollection("Rafi");
-const charactersSorter = new Sorter(charactersCollection);
-charactersSorter.sort();
+// const charactersSorter = new Sorter(charactersCollection);
+// charactersSorter.sort();
+charactersCollection.sort();
 console.log(charactersCollection.data);
 
 const linkedList = new LinkedList();
@@ -18,6 +20,7 @@ linkedList.add(34);
 linkedList.add(5);
 linkedList.add(-6);
 linkedList.add(2);
-const linkedListSorter = new Sorter(linkedList);
-linkedListSorter.sort();
+// const linkedListSorter = new Sorter(linkedList);
+// linkedListSorter.sort();
+linkedList.sort();
 linkedList.print();
