2d0f1d6d423639d5a9a9db9206370e77e543cdb5
rafi-pawzeeble <rafi@pawzeeble.coom>
2025-07-20 10:47:29 +0530

    106. Reading CSV Files

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,1 +1,9 @@
 import fs from "fs";
+
+const matches = fs
+	.readFileSync("football.csv", {
+		encoding: "utf-8",
+	})
+	.split("\n")
+	.map((row: string): string[] => row.split(","));
+console.log(matches);
