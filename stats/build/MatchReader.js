9a3dc47c3129bf6c0d6446868d45c5ec99b1ecdb
rafi-pawzeeble <rafi@pawzeeble.coom>
2025-07-20 17:43:01 +0530

    119. Creating Abstract Classes

---
+++ stats/build/MatchReader.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const CsvFileReader_1 = require("./CsvFileReader");
+const utils_1 = require("./utils");
+class MatchReader extends CsvFileReader_1.CsvFileReader {
+    mapRow(row) {
+        return [
+            (0, utils_1.dateStringToDate)(row[0]),
+            row[1],
+            row[2],
+            parseInt(row[3]),
+            parseInt(row[4]),
+            row[5],
+            row[6],
+        ];
+    }
+}
