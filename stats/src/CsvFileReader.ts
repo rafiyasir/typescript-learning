4dc8aed5b5d5781c3db339d36db7b8799c9c57b0
rafi-pawzeeble <rafi@pawzeeble.coom>
2025-07-20 18:31:38 +0530

    122. Alternate Refactor

--- stats/src/CsvFileReader.ts
+++ stats/src/CsvFileReader.ts
@@ -1,10 +1,12 @@
 import fs from "fs";
+import { dateStringToDate } from "./utils";
+import { MatchResut } from "./MatchResult";
 
-export abstract class CsvFileReader<T> {
-	data: T[] = [];
+type MatchData = [Date, string, string, number, number, MatchResut, string];
+export class CsvFileReader {
+	data: MatchData[] = [];
 
 	constructor(public filename: string) {}
-	abstract mapRow(row: string[]): T;
 	read(): void {
 		this.data = fs
 			.readFileSync(this.filename, {
@@ -12,6 +14,16 @@ export abstract class CsvFileReader<T> {
 			})
 			.split("\n")
 			.map((row: string): string[] => row.split(","))
-			.map(this.mapRow);
+			.map((row: string[]): MatchData => {
+				return [
+					dateStringToDate(row[0]),
+					row[1],
+					row[2],
+					parseInt(row[3]),
+					parseInt(row[4]),
+					row[5] as MatchResut,
+					row[6],
+				];
+			});
 	}
 }
