9e6610399cc0e8f92e1e4d9ce3e308a26f5b1db3
Rafi Uzzaman <rafiyasir9@gmail.com>
2025-07-20 17:43:01 +0530

    119. Creating Abstract Classes

--- stats/src/CsvFileReader.ts
+++ stats/src/CsvFileReader.ts
@@ -1,12 +1,12 @@
 import fs from "fs";
-import { dateStringToDate } from "./utils";
 import { MatchResut } from "./MatchResult";
 
 type MatchData = [Date, string, string, number, number, MatchResut, string];
-export class CsvFileReader {
+export abstract class CsvFileReader {
 	data: MatchData[] = [];
 
 	constructor(public filename: string) {}
+	abstract mapRow(row: string[]): MatchData;
 	read(): void {
 		this.data = fs
 			.readFileSync(this.filename, {
@@ -15,17 +15,5 @@ export class CsvFileReader {
 			.split("\n")
 			.map((row: string): string[] => row.split(","))
 			.map(this.mapRow);
-	}
-
-	mapRow(row: string[]): MatchData {
-		return [
-			dateStringToDate(row[0]),
-			row[1],
-			row[2],
-			parseInt(row[3]),
-			parseInt(row[4]),
-			row[5] as MatchResut,
-			row[6],
-		];
 	}
 }
