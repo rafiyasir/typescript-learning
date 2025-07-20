c7c01cf445e08dffc3d03fbc677e58e4063a1986
rafi-pawzeeble <rafi@pawzeeble.coom>
2025-07-20 19:43:48 +0530

    126. Updating Reader References

--- stats/build/index.js
+++ stats/build/index.js
@@ -1,15 +1,17 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const CsvFileReader_1 = require("./CsvFileReader");
 const MatchReader_1 = require("./MatchReader");
 const MatchResult_1 = require("./MatchResult");
-const reader = new MatchReader_1.MatchReader("football.csv");
-reader.read();
-console.log(reader.data);
+const csvFileReader = new CsvFileReader_1.CsvFileReader("football.csv");
+const matchReader = new MatchReader_1.MatchReader(csvFileReader);
+matchReader.load();
+// console.log(reader.data);
 // const homeWin = "H";
 // const awayWin = "A";
 // const draw = "D";
 let manUnitedWins = 0;
-for (let match of reader.data) {
+for (let match of matchReader.matches) {
     if (match[1] === "Man United" && match[5] === MatchResult_1.MatchResut.HomeWin) {
         manUnitedWins++;
     }
