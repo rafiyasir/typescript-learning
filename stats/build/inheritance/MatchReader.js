ef4da8c256146daf5874968a79d81b0d214ef372
rafi-pawzeeble <rafi@pawzeeble.coom>
2025-07-21 10:23:54 +0530

    131. A Composition-Based Approach

--- stats/build/inheritance/MatchReader.js
+++ stats/build/inheritance/MatchReader.js
@@ -1,19 +1,18 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MatchReader = void 0;
-const CsvFileReader_1 = require("./CsvFileReader");
-const utils_1 = require("./utils");
-class MatchReader extends CsvFileReader_1.CsvFileReader {
-    mapRow(row) {
-        return [
-            (0, utils_1.dateStringToDate)(row[0]),
-            row[1],
-            row[2],
-            parseInt(row[3]),
-            parseInt(row[4]),
-            row[5],
-            row[6],
-        ];
-    }
-}
-exports.MatchReader = MatchReader;
+// import { CsvFileReader } from "./CsvFileReader";
+// import { MatchResut } from "./MatchResult";
+// import { dateStringToDate } from "./utils";
+// type MatchData = [Date, string, string, number, number, MatchResut, string];
+// export class MatchReader extends CsvFileReader<MatchData> {
+// 	mapRow(row: string[]): MatchData {
+// 		return [
+// 			dateStringToDate(row[0]),
+// 			row[1],
+// 			row[2],
+// 			parseInt(row[3]),
+// 			parseInt(row[4]),
+// 			row[5] as MatchResut,
+// 			row[6],
+// 		];
+// 	}
+// }
