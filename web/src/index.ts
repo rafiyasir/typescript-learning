09e45ed722a0292abeae5878802f908da78d0057
rafi-pawzeeble <rafi@pawzeeble.coom>
2025-07-23 11:41:56 +0530

    154.triggering event callb ack

--- web/src/index.ts
+++ web/src/index.ts
@@ -7,6 +7,12 @@ console.log(user.get("age"));
 user.set({ name: "RAFI" });
 console.log(user.get("name"));
 
-user.on("change", () => {});
+user.on("change", () => {
+	console.log("Change #1");
+});
+user.on("change", () => {
+	console.log("Change #2");
+});
 
 console.log(user);
+user.trigger("change");
