48a25d4d8e42357e01aa9edb8f16ef1c5cb23603
rafi-pawzeeble <rafi@pawzeeble.coom>
2025-07-23 20:53:06 +0530

    168.connecting sync back to user

--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -1,13 +1,16 @@
 import axios, { AxiosResponse } from "axios";
 import { Eventing } from "./Eventing";
+import { Sync } from "./Sync";
 
 export interface UserProps {
 	id?: number;
 	name?: string;
 	age?: number;
 }
+const rootUrl: string = "http://localhost:3000/user";
 export class User {
 	public events: Eventing = new Eventing();
+	public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
 
 	constructor(private data: UserProps) {}
 
