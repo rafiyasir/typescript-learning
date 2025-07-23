b5c8b121a9678c8e15b5b5a7cfc0c9d0ab13f2ff
rafi-pawzeeble <rafi@pawzeeble.coom>
2025-07-23 10:52:33 +0530

    149.optional interface properties

--- web/src/index.ts
+++ web/src/index.ts
@@ -3,3 +3,6 @@ import { User } from "./models/User";
 const user = new User({ name: "Rafi", age: 30 });
 console.log(user.get("name"));
 console.log(user.get("age"));
+
+user.set({ name: "RAFI" });
+console.log(user.get("name"));
