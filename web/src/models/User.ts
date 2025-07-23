dda87af6401c38dc00d4f8c92033a3cf6ce5b643
Rafi Uzzaman <rafiyasir9@gmail.com>
2025-07-23 14:01:50 +0530

    158.adding fetch functionality

--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -1,3 +1,5 @@
+import axios, { AxiosResponse } from "axios";
+
 interface UserProps {
 	name?: string;
 	age?: number;
@@ -31,5 +33,13 @@ export class User {
 		handlers.forEach((callback) => {
 			callback();
 		});
+	}
+
+	fetch(): void {
+		axios
+			.get(`http://localhost:3000/users/${this.get("id")}`)
+			.then((response: AxiosResponse): void => {
+				this.set(response.data);
+			});
 	}
 }
