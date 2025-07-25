da1d59b9d140b78eac9a65d99c62e40886b53147
rafi-pawzeeble <rafi@pawzeeble.coom>
2025-07-25 09:33:33 +0530

    186.final user refactor

--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -1,3 +1,6 @@
+import { ApiSync } from "./ApiSync";
+import { Attributes } from "./Attributes";
+import { Eventing } from "./Eventing";
 import { Model } from "./Model";
 
 export interface UserProps {
@@ -6,4 +9,12 @@ export interface UserProps {
 	age?: number;
 }
 const rootUrl: string = "http://localhost:3000/users";
-export class User extends Model<UserProps> {}
+export class User extends Model<UserProps> {
+	static buildUser(attrs: UserProps): User {
+		return new User(
+			new Attributes<UserProps>(attrs),
+			new Eventing(),
+			new ApiSync<UserProps>(rootUrl)
+		);
+	}
+}
