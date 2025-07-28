66f910d9e30c842df2754fbf37c2e99332cd9aa9
Rafi Uzzaman <rafiyasir9@gmail.com>
2025-07-28 11:11:15 +0530

    222. Basci Routes with Express

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,15 @@
-console.log("Hi");
+import express, { Request, Response } from "express";
+
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+	res.send(`
+    <div>
+    <h1>Hi there</h1>
+    </div>
+    `);
+});
+
+app.listen(3000, () => {
+	console.log("Server listening on port 3000");
+});
