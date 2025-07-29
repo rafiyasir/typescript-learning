c4a15d2b64052432bd92ab049ea9c0ec9b984823
Rafi Uzzaman <rafiyasir9@gmail.com>
2025-07-29 18:26:31 +0530

    240.Decorator Factories

--- decorators/decorator.ts
+++ decorators/decorator.ts
@@ -5,22 +5,24 @@ class Boat {
 		return `The boats color is ${this.color}`;
 	}
 
-	@logError
+	@logError("HHellow Error")
 	pilot(): void {
 		throw new Error();
 		console.log("swish");
 	}
 }
 
-function logError(target: any, key: string, desc: PropertyDescriptor) {
-	const method = desc.value;
+function logError(errorMsg: string) {
+	return function (target: any, key: string, desc: PropertyDescriptor) {
+		const method = desc.value;
 
-	desc.value = function () {
-		try {
-			method();
-		} catch (error) {
-			console.log("Oops, boat was sunk");
-		}
+		desc.value = function () {
+			try {
+				method();
+			} catch (error) {
+				console.log(errorMsg);
+			}
+		};
 	};
 }
 
