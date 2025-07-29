8990f5ee2d2c20795777bb4807b5c587b7675ea4
rafi-pawzeeble <rafi@pawzeeble.coom>
2025-07-29 12:47:32 +0530

    231.Protecting Route

--- server/build/routes/loginRoutes.js
+++ server/build/routes/loginRoutes.js
@@ -2,6 +2,16 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.router = void 0;
 const express_1 = require("express");
+function requireAuth(req, res, next) {
+    if (req.session && req.session.login) {
+        next();
+        return;
+    }
+    else {
+        res.status(403);
+        res.send("Not Permitted");
+    }
+}
 const router = (0, express_1.Router)();
 exports.router = router;
 router.get("/login", (req, res) => {
@@ -50,4 +60,7 @@ router.get("/", (req, res) => {
 router.get("/logout", (req, res) => {
     req.session = undefined;
     res.redirect("/");
+});
+router.get("/protected", requireAuth, (req, res) => {
+    res.send("Welcome");
 });
