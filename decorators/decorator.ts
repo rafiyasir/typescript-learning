a7d8466ffaa4579a523a13a727566d4989a3646d
rafi-pawzeeble <rafi@pawzeeble.coom>
2025-07-30 20:30:49 +0530

    242. More on Decorators

--- decorators/decorator.ts
+++ decorators/decorator.ts
@@ -1,16 +1,32 @@
+@classDecorator
 class Boat {
 	@testDecorator
 	color: string = "Red";
 
+	@testDecorator
 	get formattedColor(): string {
 		return `The boats color is ${this.color}`;
 	}
 
 	@logError("HHellow Error")
-	pilot(): void {
-		throw new Error();
-		console.log("swish");
+	pilot(
+		@parameterDecorator speed: string,
+		@parameterDecorator stairing: boolean
+	): void {
+		if (speed === "fast") {
+			console.log("swish");
+		} else {
+			console.log("-----");
+		}
 	}
+}
+
+function classDecorator(constructor: typeof Boat) {
+	console.log(constructor);
+}
+
+function parameterDecorator(target: any, key: string, index: number) {
+	console.log(key, index);
 }
 
 function testDecorator(target: any, key: string) {
