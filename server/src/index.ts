c3fde8999cadba1082818d25f400e82d35374942
Rafi Uzzaman <rafiyasir9@gmail.com>
2025-07-31 16:20:15 +0530

    254. POC

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,12 +2,16 @@ import express, { Request, Response } from "express";
 import { router } from "./routes/loginRoutes";
 import bodyParser from "body-parser";
 import cookieSession from "cookie-session";
+import { router as controllerRouter } from "./decorators/controller";
+
+import "./controllers/LoginController";
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieSession({ keys: ["lsdfdfddf"] }));
 app.use(router);
+app.use(controllerRouter);
 
 app.listen(3000, () => {
 	console.log("Server listening on port 3000");
