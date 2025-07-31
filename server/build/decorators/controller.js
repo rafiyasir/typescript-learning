2cd4a4782db2e959226c87ee51a4ac3a084e2440
Rafi Uzzaman <rafiyasir9@gmail.com>
2025-07-31 18:08:25 +0530

    258. metadata keys

--- server/build/decorators/controller.js
+++ server/build/decorators/controller.js
@@ -3,14 +3,17 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.controller = controller;
 require("reflect-metadata");
 const AppRouter_1 = require("../../AppRouter");
+const MetadataKeys_1 = require("./MetadataKeys");
 function controller(routePrefix) {
     return function (target) {
         const router = AppRouter_1.AppRouter.getInstance();
         Object.getOwnPropertyNames(target.prototype).forEach((key) => {
             const routeHandler = target.prototype[key];
-            const path = Reflect.getMetadata("path", target.prototype, key);
-            const method = Reflect.getMetadata("method", target.prototype, key);
-            router[method](`${routePrefix}${path}`, routeHandler);
+            const path = Reflect.getMetadata(MetadataKeys_1.MetadataKeys.path, target.prototype, key);
+            const method = Reflect.getMetadata(MetadataKeys_1.MetadataKeys.method, target.prototype, key);
+            if (path) {
+                router[method](`${routePrefix}${path}`, routeHandler);
+            }
         });
     };
 }
--- server/build/decorators/routes.js
+++ server/build/decorators/routes.js
@@ -3,11 +3,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.del = exports.put = exports.patch = exports.post = exports.get = void 0;
 require("reflect-metadata");
 const Methods_1 = require("./Methods");
+const MetadataKeys_1 = require("./MetadataKeys");
 function routeBinder(method) {
     return function (path) {
         return function (target, key, desc) {
-            Reflect.defineMetadata("path", path, target, key);
-            Reflect.defineMetadata("method", method, target, key);
+            Reflect.defineMetadata(MetadataKeys_1.MetadataKeys.path, path, target, key);
+            Reflect.defineMetadata(MetadataKeys_1.MetadataKeys.method, method, target, key);
         };
     };
 }
