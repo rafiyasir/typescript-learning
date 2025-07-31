53b183050152b5add5c62b4b59e1b91537de0759
rafi-pawzeeble <rafi@pawzeeble.coom>
2025-07-31 18:08:25 +0530

    258. metadata keys

--- server/src/decorators/controller.ts
+++ server/src/decorators/controller.ts
@@ -1,19 +1,26 @@
 import "reflect-metadata";
 import { AppRouter } from "../../AppRouter";
 import { Methods } from "./Methods";
+import { MetadataKeys } from "./MetadataKeys";
 
 export function controller(routePrefix: string) {
 	return function (target: Function) {
 		const router = AppRouter.getInstance();
 		Object.getOwnPropertyNames(target.prototype).forEach((key) => {
 			const routeHandler = target.prototype[key];
-			const path = Reflect.getMetadata("path", target.prototype, key);
-			const method: Methods = Reflect.getMetadata(
-				"method",
+			const path = Reflect.getMetadata(
+				MetadataKeys.path,
 				target.prototype,
 				key
 			);
-			router[method](`${routePrefix}${path}`, routeHandler);
+			const method: Methods = Reflect.getMetadata(
+				MetadataKeys.method,
+				target.prototype,
+				key
+			);
+			if (path) {
+				router[method](`${routePrefix}${path}`, routeHandler);
+			}
 		});
 	};
 }
