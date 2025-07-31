4135e51e6f48b11af964c66d65ce16789ff8e42a
rafi-pawzeeble <rafi@pawzeeble.coom>
2025-07-31 20:51:23 +0530

    262. automated validation

--- server/src/decorators/controller.ts
+++ server/src/decorators/controller.ts
@@ -2,7 +2,23 @@ import "reflect-metadata";
 import { AppRouter } from "../../AppRouter";
 import { Methods } from "./Methods";
 import { MetadataKeys } from "./MetadataKeys";
+import { NextFunction, Request, RequestHandler, Response } from "express";
 
+function bodyValidators(keys: string): RequestHandler {
+	return function (req: Request, res: Response, next: NextFunction) {
+		if (!req.body) {
+			res.status(422).send("Invalid Request");
+			return;
+		}
+		for (let key of keys) {
+			if (!req.body[key]) {
+				res.status(422).send("Invalid Reques");
+				return;
+			}
+		}
+		next();
+	};
+}
 export function controller(routePrefix: string) {
 	return function (target: Function) {
 		const router = AppRouter.getInstance();
@@ -21,8 +37,18 @@ export function controller(routePrefix: string) {
 			const middlewares =
 				Reflect.getMetadata(MetadataKeys.middleware, target.prototype, key) ||
 				[];
+
+			const requiredBodyProps =
+				Reflect.getMetadata(MetadataKeys.validator, target.prototype, key) ||
+				[];
+			const validator = bodyValidators(requiredBodyProps);
 			if (path) {
-				router[method](`${routePrefix}${path}`, ...middlewares, routeHandler);
+				router[method](
+					`${routePrefix}${path}`,
+					...middlewares,
+					validator,
+					routeHandler
+				);
 			}
 		});
 	};
