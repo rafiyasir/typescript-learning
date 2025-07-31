bfb23504929312c23ddccb3bacf2039df6d98ef1
rafi-pawzeeble <rafi@pawzeeble.coom>
2025-07-31 16:20:15 +0530

    254. POC

--- server/src/decorators/controller.ts
+++ server/src/decorators/controller.ts
@@ -1,9 +1,14 @@
 import "reflect-metadata";
+import express from "express";
+
+export const router = express.Router();
+
 export function controller(routePrefix: string) {
 	return function (target: Function) {
 		Object.getOwnPropertyNames(target.prototype).forEach((key) => {
 			const routeHandler = target.prototype[key];
 			const path = Reflect.getMetadata("path", target.prototype, key);
+			router.get(`${routePrefix}${path}`, routeHandler);
 		});
 	};
 }
