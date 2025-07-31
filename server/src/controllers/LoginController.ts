ca29cd3c4cc30b5e8abd626970cf6431b6580c3a
rafi-pawzeeble <rafi@pawzeeble.coom>
2025-07-31 12:14:58 +0530

    250. lets refactor

---
+++ server/src/controllers/LoginController.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from "express";
+
+@controller("/")
+class LoginController {
+	@get("/login")
+	getLogin(req: Request, res: Response): void {
+		res.send(`
+      <form method="POST">
+        <p>
+          <label>Email</label>
+          <input name="email" />
+        </p>
+        <p>
+          <label>Password</label>
+          <input name="password" type="password" />
+        </p>
+        <button>Submit</button>
+      </form>
+    `);
+	}
+}
