ce07ee4afced1a4e31c6585abf354711aeadd982
Rafi Uzzaman <rafiyasir9@gmail.com>
2025-07-31 16:37:33 +0530

    255. a few fixup

--- server/build/decorators/controller.js
+++ server/build/decorators/controller.js
@@ -1,19 +1,15 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.router = void 0;
 exports.controller = controller;
 require("reflect-metadata");
-const express_1 = __importDefault(require("express"));
-exports.router = express_1.default.Router();
+const AppRouter_1 = require("../../AppRouter");
 function controller(routePrefix) {
     return function (target) {
+        const router = AppRouter_1.AppRouter.getInstance();
         Object.getOwnPropertyNames(target.prototype).forEach((key) => {
             const routeHandler = target.prototype[key];
             const path = Reflect.getMetadata("path", target.prototype, key);
-            exports.router.get(`${routePrefix}${path}`, routeHandler);
+            router.get(`${routePrefix}${path}`, routeHandler);
         });
     };
 }
--- server/src/index.ts
+++ server/src/index.ts
@@ -2,16 +2,16 @@ import express, { Request, Response } from "express";
 import { router } from "./routes/loginRoutes";
 import bodyParser from "body-parser";
 import cookieSession from "cookie-session";
-import { router as controllerRouter } from "./decorators/controller";
 
 import "./controllers/LoginController";
+import { AppRouter } from "../AppRouter";
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieSession({ keys: ["lsdfdfddf"] }));
 app.use(router);
-app.use(controllerRouter);
+app.use(AppRouter.getInstance());
 
 app.listen(3000, () => {
 	console.log("Server listening on port 3000");
