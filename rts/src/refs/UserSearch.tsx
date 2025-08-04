ae9b504f0a801aaee4a3289c385f0856f7040e68
Rafi Uzzaman <rafiyasir9@gmail.com>
2025-08-04 11:59:01 +0530

    307. refs

---
+++ rts/src/refs/UserSearch.tsx
@@ -0,0 +1,37 @@
+import { useRef, useState } from "react";
+
+const users = [
+  { name: "Rafi", age: 20 },
+  { name: "Rafi1", age: 25 },
+  { name: "Rafi3", age: 30 },
+];
+
+const UserSearch: React.FC = () => {
+  const inputRef = useRef<HTMLInputElement | null>(null);
+  const [name, setName] = useState("");
+  const [user, setUser] = useState<{ name: string; age: number } | undefined>();
+
+  const onClick = () => {
+    const foundUser = users.find((user) => {
+      return user.name === name;
+    });
+    setUser(foundUser);
+  };
+  return (
+    <div>
+      <h3>User Search</h3>
+      <input
+        ref={inputRef}
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <button onClick={onClick}>Find User</button>
+      <div>
+        {user && user.name}
+        {user && user.age}
+      </div>
+    </div>
+  );
+};
+
+export default UserSearch;
