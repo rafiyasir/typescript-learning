3ee21c32a2b95af86462f45988491a1888c2bd1f
Rafi Uzzaman <rafiyasir9@gmail.com>
2025-08-04 11:38:31 +0530

    303. type union in state

--- rts/src/state/UserSearch.tsx
+++ rts/src/state/UserSearch.tsx
@@ -6,14 +6,25 @@ const users = [
   { name: "Rafi3", age: 30 },
 ];
 
-const onClick = () => {};
 const UserSearch: React.FC = () => {
   const [name, setName] = useState("");
+  const [user, setUser] = useState<{ name: string; age: number } | undefined>();
+
+  const onClick = () => {
+    const foundUser = users.find((user) => {
+      return user.name === name;
+    });
+    setUser(foundUser);
+  };
   return (
     <div>
       <h3>User Search</h3>
       <input value={name} onChange={(e) => setName(e.target.value)} />
       <button onClick={onClick}>Find User</button>
+      <div>
+        {user && user.name}
+        {user && user.age}
+      </div>
     </div>
   );
 };
