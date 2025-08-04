81372c6a120668bfb0f45b99aefe2362db82231d
Rafi Uzzaman <rafiyasir9@gmail.com>
2025-08-04 23:17:24 +0530

    326. initial state

--- redux-ts/src/state/reducers/repositoriesReducer.ts
+++ redux-ts/src/state/reducers/repositoriesReducer.ts
@@ -7,9 +7,14 @@ interface  RepositoriesState {
     data: string[]
 }
 
+const initialState = {
+    loading:false,
+    error:null,
+    data: []
+}
 
 
-const reducer = (state:RepositoriesState, action: Action ):RepositoriesState => {
+const reducer = (state:RepositoriesState=initialState, action: Action ):RepositoriesState => {
     switch(action.type){
         case ActionType.SEARCH_REPOSITORIES:
             return {loading: true, error: null, data: []}
